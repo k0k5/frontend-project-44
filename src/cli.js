b2b70bde92e774c46c54eb8bc0aff67cad5e5155
aaa <sdfsdf@dsfsdf.com>
2025-05-15 15:53:51 +0300

    почти сделан 4 пункт

---
+++ src/cli.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+import readlineSync from 'readline-sync';
+
+function greetUser(){
+    const userName = readlineSync.question('May I have your name? ');
+    console.log(`Hello, ${userName}!`);
+  }
+  
+export default greetUser;
