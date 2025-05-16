1795be232f91ce7c0bcf447484ffd6146edab4bf
aaa <sdfsdf@dsfsdf.com>
2025-05-16 10:25:26 +0300

    калькулятор

---
+++ bin/brain-calculate.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+import {calcNums, generationNumber,generationMathOperator } from '../src/helpers/helpers.js';
+import runGame from '../src/runGame.js';
+
+const generateCalcQuestion = () => {
+    const a = generationNumber();
+    const b = generationNumber();
+    const operator = generationMathOperator();
+
+    const question = `${a} ${operator} ${b}`;
+    const correctAnswer = String(calcNums(a, b, operator));
+
+    return [question, correctAnswer];
+
+    };
+
+    const description = 'What is the result of the expression?';
+    runGame(description, generateCalcQuestion);
--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -12,3 +12,32 @@ export const isEven = (n) =>{
 }
 
 
+
+export const generationMathOperator = () => {
+    const num = Math.floor(Math.random() * 3);
+  
+    switch (num) {
+      case 0:
+        return '+';
+  
+      case 1:
+        return '-';
+  
+      default:
+        return '*';
+    }
+  };
+  
+
+  export const calcNums = (num1, num2, operator) => {
+    switch (operator) {
+      case '+':
+        return num1 + num2;
+  
+      case '-':
+        return num1 - num2;
+  
+      case '*':
+        return num1 * num2;
+    }
+  };
