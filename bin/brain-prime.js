115adeafa92d6b35a8609f9a5d6398b85f4eac0a
sitok1 <dododon111111@gmail.com>
2025-06-06 20:56:05 +0300

    dfgfd

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,16 +1,14 @@
 #!/usr/bin/env node
 
-import { generationNumber, isPrime } from "../src/helpers/helpers.js";
-import runGame from "../src/runGame.js";
+import runGame from '../src/runGame.js'
+import { generationNumber, isPrime } from '../src/helpers/helpers.js'
 
-const generatePrimeQuestion = () =>{
-    const a = generationNumber();
-
-    const question = `${a}`;
-    const correctAnswer = isPrime(a) ? 'yes' : 'no';
-
-    return [question, correctAnswer];
+const generatePrimeQuestion = () => {
+  const num = generationNumber()
+  const question = num
+  const correctAnswer = isPrime(num) ? 'yes' : 'no'
+  return [question, correctAnswer]
 }
 
-const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
-runGame(description, generatePrimeQuestion);
+const description = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+runGame(description, generatePrimeQuestion)
